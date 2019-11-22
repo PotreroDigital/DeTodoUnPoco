60370189ec8aee5692ab046953194a8c252fde9b
Brian <57192655+thormunt@users.noreply.github.com>
2019-11-22 18:35:52 -0300

    Add files via upload

---
+++ act1 (1).php
@@ -0,0 +1,34 @@
+<?php
+
+//for ($i=1; $i < 101 ; $i=$i+1) {
+  //echo "$i <br>";
+//}
+//for ($i=100; $i > 0 ; $i=$i + -1) {
+  //echo "$i <br>";
+//}
+//for ($i=0; $i < 102 ; $i=$i + 2) {
+//  echo "$i <br>";
+//}
+//for ($i=1; $i < 102 ; $i=$i+2) {
+//echo "$i <br>";
+//}
+
+//for ($i=1; $i < 101 ; $i=$i+2) {
+  //echo "$i";
+//$resultado=0;
+//for ($i= 1; $i < 21 ; $i=$i + 1) {
+  //$resultado+ $resultado = $i;
+//echo "$resultado <br>";
+//$resultado=0;
+//$x=0;
+for ($i=2; $i <21 ; $i=$i+2) {
+$resultado=$x+$i;
+  echo "$x + $i = $resultado <br>";
+  $x=$resultado;
+}
+
+
+
+
+
+ ?>
